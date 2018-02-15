5a845a026e42d534ab13cfcf5466dd2ab74233b0
Giovanni Bussi <giovanni.bussi@gmail.com>
2018-02-15 20:15:40 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,53 @@
+#/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#   Copyright (c) 2011-2016 The plumed team
+#   (see the PEOPLE file at the root of the distribution for a list of names)
+#
+#   See http://www.plumed.org for more information.
+#
+#   This file is part of plumed, version 2.
+#
+#   plumed is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Lesser General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   plumed is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Lesser General Public License for more details.
+#
+#   You should have received a copy of the GNU Lesser General Public License
+#   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#
+# This python routine builds an interface between plumed and python using cython
+#
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+import subprocess
+import os
+
+# perhaps plumed name should be imported from environment.
+# it should be used to set:
+# - libraries=[program_name "Wrapper"] 
+# - include_dir in such a way to make the statement
+#       cdef extern from "plumed/wrapper/Plumed.h" :
+#   in cplumed.pxd more general
+
+setup(
+  name='plumed-py',
+  description='Python interface to PLUMED',
+  author='Gareth A. Tribello',
+  author_email='plumed-users@googlegroups.com',
+  url='http://www.plumed.org',
+  ext_modules = cythonize([
+                  Extension( name="plumed",
+                             sources=["plumed.pyx"],
+                             libraries=["plumedWrapper"],
+                             language="c++",
+                             include_dirs=[numpy.get_include()]
+                           )
+                          ])
+)
